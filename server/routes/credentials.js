3aff33497c76c3c6767625f71e2b5643603fbcad
Mehul Singh <mehulsingh072001@gmail.com>
2021-12-01 01:50:47 +0530

    added populate but not working properly

--- server/routes/credentials.js
+++ server/routes/credentials.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const verify = require('../verifyToken')
 const Credentials = require('../models/credentials.model')
+const User = require('../models/user.model')
 
 const router = express.Router()
 
@@ -16,13 +17,20 @@ router.get('/credentials', verify, async (req, res) => {
     }
 })
 
+
 router.post('/credentials', verify, async (req, res) => {
     const credentials = new Credentials({
         username: req.body.username,
         password: req.body.password
     })
     try {
-        await credentials.save()
+        // save credentials to database
+        const savedCredentials = await credentials.save()
+
+        //save the user to databse
+        const user = await User.findOne({name: req.body.name}).populate('credentials')
+        res.json(user)
+
         return res.status(200).json({
             message: "Successfully added credentials!"
         })
