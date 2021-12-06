5c36c7b9618b85381de4b9b2799df7d66e463fae
Mehul Singh <mehulsingh072001@gmail.com>
2021-12-06 19:41:19 +0530

    can detect all websites but gmail

--- extensions/passman/background.js
+++ extensions/passman/background.js
@@ -1,9 +1,20 @@
 var ary = [];
-var inputs = document.getElementsByTagName("input");
+var inputs = document.querySelectorAll("input");
+
 for (var i=0; i<inputs.length; i++){
     if(inputs[i].type.toLowerCase() === "password"){
+        inputs[i-1].onchange = (e) => {
+            console.log(e.target.value)
+        }
         inputs[i].onchange = (e) => {
             console.log(e.target.value)
         }
     }
+    // else if(inputs[i].type.toLowerCase() === "password"){
+    //     inputs[i].onchange = (e) => {
+    //         console.log(e.target.value)
+    //     }
+    // }
 }
+
+// console.log(pass)
