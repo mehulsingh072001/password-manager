9e40674855ea25f6be97fc5717bb60ac347b6e0d
Mehul Singh <mehulsingh072001@gmail.com>
2021-12-30 14:17:42 +0530

    added helper function

---
+++ server/app/Helpers.php
@@ -0,0 +1,6 @@
+<?php
+
+function helloWorldHelper($data)
+{
+    return $data;
+}
--- server/app/Http/Controllers/CredentialsController.php
+++ server/app/Http/Controllers/CredentialsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Helper\Helper;
 use App\Models\Credentials;
 use App\Models\Folders;
 use App\Models\User;
@@ -51,45 +52,8 @@ class CredentialsController extends Controller
             return response($response);
     }
     public function loadCsv($folderId){
-        $folder = Folders::find($folderId);
         $file = storage_path('app/public/logins.csv'); //--> laravel helper, but you can use any path here
-
-        // file() loads each row as an array value, then array map uses the 'str_getcsv' callback to 
-          $csv = array_map('str_getcsv', file($file)); 
-
-        //  array_walk - "walks" through each item of the array and applies the call back function. the & in "&row" means that alterations to $row actually change the original $csv array, rather than treating it as immutable (*sort of immutable...)
-          array_walk($csv, function(&$row) use ($csv) {
-         
-            // array_combine takes the header row ($csv[0]) and uses it as array keys for each column in the row
-            $row = array_combine($csv[0], $row); 
-          });
-          
-        array_shift($csv); # removes now very redundant column header --> contains {'col_1':'col_1', 'col_2':'col_2'...}
-        $json = json_encode($csv);
-
-        $data = json_decode($json, true);
-        foreach($data as $url){
-            $username = json_encode($url['username']);
-            $password = json_encode($url['password']);
-            $url = json_encode($url['url']);
-
-            $credf = $folder->credentials()->create(['username' => $username, 'url' => $url, 'password' => $password]);
-        }
-        /* for($i=0; $i < sizeof($data); $i++){ */
-        /*     $url = $data[$i]['url']; */
-        /*     $username = $data[$i]['username']; */
-        /*     $password = $data[$i]['password']; */
-        /*     $a = [ */
-        /*         $username, $password, $url */
-        /*     ]; */
-        /*     $credf = $folder->credentials()->create($a); */
-        /* } */
-        /* array_push($arr, $a); */
-
-        $response = [
-            'f' => $credf
-        ];
-        return $response;
+        return helloWorldHelper($file);
     }
 
     /**
